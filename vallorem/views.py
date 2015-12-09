224d929d195bea36b53d848ca4be6d013788e2a4
Ludovic Coues <gkr@jellycopter.net>
2015-12-09 11:16:07 +0100

    Add site map, fix white space

--- vallorem/views.py
+++ vallorem/views.py
@@ -43,6 +43,11 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+def has_no_empty_params(rule):
+    """return True if the rule can be used without arguments"""
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(arguments) <= len(defaults)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -67,7 +72,7 @@ def logout():
 
 @app.route('/')
 def home():
-	return redirect('index')
+    return redirect('index')
 
 @app.route('/index')
 def index():
@@ -104,3 +109,14 @@ def categorie(action=None):
         return render_template('ajoutCategorie.html')
     else:
         return "Action non valide"
+
+@app.route('/site-map')
+def site_map():
+    """ Display the list of rules reachable with a browser and without
+    parameters """
+    links = []
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    return render_template('site_map.html', urls=links)
