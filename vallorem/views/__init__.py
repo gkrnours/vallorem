11a7159aa679f48cbb9b6627713acb9396725920
root <erwan1805@gmail.com>
2015-12-09 07:11:48 -0800

    ajout du fichier categori dans view

--- vallorem/views/__init__.py
+++ vallorem/views/__init__.py
@@ -11,6 +11,7 @@ from vallorem.form import PageForm,CategorieForm
 from vallorem import app
 from vallorem.views import login
 from vallorem.views import page
+from vallorem.views import categorie
 
 def connect_db():
     """Connects to the specific database."""
@@ -71,33 +72,6 @@ def dashboard():
 def config():
     onglet = {'sys': 'selected'}
     return render_template('page/page.html', page=onglet)
-
-@app.route('/categorie/')
-@app.route('/categorie/<action>', methods=['GET', 'POST'])
-def categorie(action=None):
-    onglet = {'categ': 'selected'}
-    form=CategorieForm()
-    return render_template('categorie/categorie.html', page=onglet)
-    
-
-@app.route('/categorie/ajout', methods=['GET', 'POST'])
-def categorieAjout():
-    form=CategorieForm()
-    onglet = {'categ': 'selected'}
-    if request.method == "POST":
-    #ecrire des codes pour ajouter input dans la base de donnees
-        form = CategorieForm(request.form)
-        flash('vous avez entré la description:'+form.description.data)
-    return render_template('categorie/ajout.html', page=onglet, form=form)
-
-@app.route('/categorie/modif', methods=['GET', 'POST'])
-def categorieModif():
-    form=CategorieForm()
-    onglet = {'categ': 'selected'}
-    if request.method == "POST":
-    #ecrire des codes pour ajouter input dans la base de donnees
-        form = CategorieForm(request.form)
-    return render_template('categorie/ajout.html', page=onglet, form=form)
 
 
 @app.route('/user/')
