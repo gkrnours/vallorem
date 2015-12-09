5e3953c6e3de7ba978fccdad4aa9c80b9e335503
root <erwan1805@gmail.com>
2015-12-09 15:25:45 -0800

    ajout d'un drop-down pour la categori d'une page

--- vallorem/form.py
+++ vallorem/form.py
@@ -1,10 +1,10 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField, SubmitField, TextAreaField,PasswordField
+from wtforms import TextField, BooleanField, SubmitField, TextAreaField,PasswordField, SelectField
 from wtforms.validators import DataRequired as Required
 
 class PageForm(Form):
 	title= TextField('title', validators=[Required()])
-	categorie=TextField('categorie', validators=[Required()])
+	categorie = SelectField(u'Categorie', choices=[('1', 'test'), ('2', 'test2'), ('3', 'test3'),('3', 'test3'),('4', 'test4')])
 	contenu=TextAreaField('contenu', validators=[Required()])
 	submit = SubmitField('Submit')
 	
