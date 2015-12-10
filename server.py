ad2244f7b6be5ad118e4fcc50d4198ac5df81acd
leodedit <leodedit@gmail.com>
2015-12-10 11:37:21 +0100

    fix import for model

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python2.7
 # -*- encoding: utf-8 -*-
 
+import argparse
 from vallorem import app
-app.run(debug=True)
 
+from vallorem.model.db import init_db
+
+
+parser = argparse.ArgumentParser(description="Vallorem")
+parser.add_argument("--initdb", dest='initdb', action="store_true")
+args = parser.parse_args()
+
+if args.initdb:
+    init_db()
+    print 'La base de données a été initialisée'
+else:
+    app.run(debug=True)
--- vallorem/model/db.py
+++ vallorem/model/db.py
@@ -1,16 +1,16 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
 from vallorem import app
+from vallorem.model import Base
 from vallorem.model import categorie, page
 
 engine = create_engine('sqlite:///db/vallorem.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
-Base = declarative_base()
+
 Base.query = db_session.query_property()
 
 
