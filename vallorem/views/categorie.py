0f23666fcbc83688c34a54562b247fa5a3e9e089
root <erwan1805@gmail.com>
2015-12-10 06:07:30 -0800

    chargement des catégories depuis la base de données

--- vallorem/views/categorie.py
+++ vallorem/views/categorie.py
@@ -6,14 +6,24 @@ from flask import Flask, request, session, redirect, url_for, flash
 from flask import render_template
 # local import
 from vallorem.form import CategorieForm
+from sqlalchemy import Table, create_engine, MetaData
+from vallorem.model.db import engine
+from vallorem.model.categorie import Categorie
+from pprint import pprint
 from vallorem import app
 
 @app.route('/categorie/')
 @app.route('/categorie/<action>', methods=['GET', 'POST'])
 def categorie(action=None):
+
+
+    categories = Categorie.query.all()
+    categDescri = []
+    for categ in categories:
+        categDescri.append(getattr(categ, "description"))
     onglet = {'categ': 'selected'}
     form=CategorieForm()
-    return render_template('categorie/categorie.html', page=onglet)
+    return render_template('categorie/categorie.html', page=onglet, categ=categDescri)
     
 
 @app.route('/categorie/ajout', methods=['GET', 'POST'])
