e9c3a8b27986791e3e205130c175f6c9d57d637d
wang <wangxiao8808@hotmail.com>
2015-12-11 15:51:52 +0100

    wang@hotmail.az

--- vallorem/form.py
+++ vallorem/form.py
@@ -3,23 +3,23 @@ from wtforms import TextField, BooleanField, SubmitField, TextAreaField,Password
 from wtforms.validators import DataRequired as Required
 
 class PageForm(Form):
-	title= TextField('title', validators=[Required()])
+	title= TextField('Title', validators=[Required()])
 	categorie = SelectField(u'Categorie', choices=[('1', 'test'), ('2', 'test2'), ('3', 'test3'),('3', 'test3'),('4', 'test4')])
-	contenu=TextAreaField('contenu', validators=[Required()])
+	contenu=TextAreaField('Contenu', validators=[Required()])
 	submit = SubmitField('Submit')
 	
 class CategorieForm(Form):
-	description= TextField('description', validators=[Required()])
+	description= TextField('Description', validators=[Required()])
 	submit = SubmitField('Submit')
 
 class UserForm(Form):
-	email= TextField('email', validators=[Required()])
-	password= PasswordField('password', validators=[Required()])
+	email= TextField('Email', validators=[Required()])
+	password= PasswordField('Password', validators=[Required()])
 	submit = SubmitField('Submit')
 
 class LoginForm(Form):
-	username= TextField('username', validators=[Required()])
-	password= PasswordField('password', validators=[Required()])
+	username= TextField('Username', validators=[Required()])
+	password= PasswordField('Password', validators=[Required()])
 	submit = SubmitField('Submit')
 
 
