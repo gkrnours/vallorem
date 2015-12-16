9dcf1bef2b9055e9eac338dda0da37ad39f6e40c
root <erwan1805@gmail.com>
2015-12-16 14:02:27 +0100

    ajout du formulaire pour doctorant

--- vallorem/form.py
+++ vallorem/form.py
@@ -1,9 +1,11 @@
 from flask.ext.wtf import Form
 
-from wtforms import TextField, BooleanField, SubmitField, TextAreaField, PasswordField, SelectField, DateField, IntegerField
+from wtforms import TextField, BooleanField, SubmitField, TextAreaField
+from wtforms import PasswordField, SelectField, DateField, IntegerField
 
 from wtforms.fields.html5 import DateField
 
+from wtforms.validators import Optional
 from wtforms.validators import DataRequired as Required
 from vallorem.model import db, Categorie, Observation, Personne, TypeFinancement
 
@@ -25,8 +27,10 @@ class PersonneForm(Form):
     nom = TextField('Nom', validators=[Required()])
     prenom = TextField('Prenom', validators=[Required()])
     nom_jf = TextField('Nom de jeune fille')
-    date_naissance = DateField('Date de naissance', validators=[Required()])
-    date_recrutement = DateField('Date de recrutement', validators=[Required()])
+    date_naissance = DateField('Date de naissance',
+            format='%d/%m/%y', validators=[Required()])
+    date_recrutement = DateField('Date de recrutement',
+            format='%d/%m/%y', validators=[Required()])
     statut = TextField('Statut', validators=[Required()])
     permanent = BooleanField('Permanent')
 
@@ -63,26 +67,19 @@ class TypeProductionForm(Form):
 
 class DoctorantForm(Form):
 
-    sujetThese = TextField('Sujet de these', validators=[Required()])
-    typeFinancement = TextField('typeFinancement', validators=[Required()])
-    personne = SelectField(u'personne', coerce=int)
-   
-    nbInscription = IntegerField('Nombre d\'inscription administrative', validators=[Required()])
-    dateSoutenance = DateField('Date de soutenance', format='%Y/%m/%d')
+    sujet_these = TextField('Sujet de these', validators=[Required()])
+    type_financement = TextField('typeFinancement', validators=[Required()])
+    nombre_ia = IntegerField('Nombre d\'inscription administrative',
+        validators=[Required()])
+    date_soutenance = DateField('Date de soutenance', validators=[Optional()])
     observation = TextAreaField('Observation', validators=[Required()])
-
-    def __init__(self, *args, **kwargs):
-        super(DoctorantForm, self).__init__(*args, **kwargs)
-        with db.session() as s:
-            personnes = s.query(Personne).all()
-        self.personne.choices = [(c.id, c.nom) for c in personnes]
 
 
 class DatePromotionForm(Form):
 
     satut = TextField('statut', validators=[Required()])
-    datePromotion = DateField('Date de promotion', format='%Y/%m/%d')
-    personne = SelectField(u'personne', coerce=int)
+    datePromotion = DateField('Date de promotion')
+
 
     def __init__(self, *args, **kwargs):
         super(DatePromotionForm, self).__init__(*args, **kwargs)
