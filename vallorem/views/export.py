2d3e7d14e04c3fafa3ce4e6aa03a4b01b8ee70a0
leodedit <leodedit@gmail.com>
2015-12-18 13:10:01 +0100

    CSV algo de malade Trop classe Export

--- vallorem/views/export.py
+++ vallorem/views/export.py
@@ -8,18 +8,16 @@ from zipfile import ZipFile
 from flask import flash, make_response
 
 from vallorem import app
-from vallorem.model import db, User, Production
+from vallorem.model import db, User, Production, Personne
 
 
 @app.route('/export/')
 def export():
-    tables = [
-        (User, ['mail', 'password']),
-        (Production, ['titre', 'description', 'extra', 'type', 'date'])
-    ]
-    out = ""
+    objs = [User, Production, Personne]
+    tables = [(x, [f.name for f in x.__table__.columns]) for x in objs]
     fichier = StringIO()
     zip = ZipFile(fichier, 'w')
+
     for obj, head in tables:
         output = StringIO()
         writer = csv.writer(output)
@@ -29,6 +27,7 @@ def export():
         for element in lst:
             writer.writerow([getattr(element, at) for at in head])
         zip.writestr("%s.csv" % obj.__name__, output.getvalue())
+
     zip.close()
     response = make_response(fichier.getvalue())
     response.headers["Content-Disposition"] = "attachment; filename=export.zip"
