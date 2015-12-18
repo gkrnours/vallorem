8c95d51f250970319621b463435f0825a6cddd94
leodedit <leodedit@gmail.com>
2015-12-18 12:25:11 +0100

    CSV opérationnel

--- vallorem/views/export.py
+++ vallorem/views/export.py
@@ -1,21 +1,35 @@
 # -*- encoding: utf-8 -*-
 # std python 
 from __future__ import unicode_literals
+from io import BytesIO as StringIO
+import csv
+from zipfile import ZipFile
 # 3rd party lib import
-from flask import Flask, request, session, redirect, url_for, flash
-from flask import render_template
-import pelican
+from flask import flash, make_response
 
 from vallorem import app
-from vallorem.model import db, Page
-from vallorem.utils import Capturing
+from vallorem.model import db, User, Production
 
 
-
-@app.route('/build/')
-def build():
-    with Capturing() as output:
-        pelican.main()
-    if output[0].startswith("Done"):
-        flash(output[0], category="success")
-    return redirect(url_for('dashboard'))
+@app.route('/export/')
+def export():
+    tables = [
+        (User, ['mail', 'password']),
+        (Production, ['titre', 'description', 'extra', 'type', 'date'])
+    ]
+    out = ""
+    fichier = StringIO()
+    zip = ZipFile(fichier, 'w')
+    for obj, head in tables:
+        output = StringIO()
+        writer = csv.writer(output)
+        with db.session() as s:
+            lst = s.query(obj).all()
+        writer.writerow(head)
+        for element in lst:
+            writer.writerow([getattr(element, at) for at in head])
+        zip.writestr("%s.csv" % obj.__name__, output.getvalue())
+    zip.close()
+    response = make_response(fichier.getvalue())
+    response.headers["Content-Disposition"] = "attachment; filename=export.zip"
+    return response
